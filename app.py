a58f6c5b7387353c0befd791486a88960d53f256
tanyz71 <69388597+tanyz71@users.noreply.github.com>
2021-03-24 13:07:19 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask,request,Response
+from botbuilder.core import BotFrameworkAdapter,BotFrameworkAdapterSettings,TurnContext,ConversationState,MemoryStorage
+from botbuilder.schema import Activity
+import asyncio
+from pic_button import SampleAnimationCard
+
+
+app = Flask(__name__)
+loop = asyncio.get_event_loop()
+
+botsettings = BotFrameworkAdapterSettings("053b477c-6f0b-4629-a12d-6bef846605ac","2da80739-ab3a-4fb1-8fa1-c4df64b087f2")
+botadapter = BotFrameworkAdapter(botsettings)
+
+CONMEMORY = ConversationState(MemoryStorage())
+botdialog = SampleAnimationCard()
+
+
+@app.route("/api/messages",methods=["POST"])
+def messages():
+    if "application/json" in request.headers["content-type"]:
+        body = request.json
+    else:
+        return Response(status = 415)
+
+    activity = Activity().deserialize(body)
+
+    auth_header = (request.headers["Authorization"] if "Authorization" in request.headers else "")
+
+    async def call_fun(turncontext):
+        await botdialog.on_turn(turncontext)
+
+    task = loop.create_task(
+        botadapter.process_activity(activity,auth_header,call_fun)
+        )
+    loop.run_until_complete(task)
+
+
+if __name__ == '__main__':
+    app.run('localhost',3978)
